27b0d4d356072a48f8070b912f7b7d48e0d9a240
vsvarost <vsvarost@gmail.com>
2022-12-03 15:57:54 +0300

    Lesson 2 Task 1 Done

--- lesson_3_task_1.py
+++ lesson_3_task_1.py
@@ -9,7 +9,8 @@ def odd_sum(lst: list):
     for i in range(1, len(lst), 2):
         s += lst[i]
     return s
-lst = [2, 3, 5, 9, 3]
+
+lst = [int(i) for i in input('Задайте список из чисел: ').split()]
 print(f'{lst} -> на нечётных индексах элементы ', end='')
 for i in range(1, len(lst), 2):
     print(lst[i], end=', ')
